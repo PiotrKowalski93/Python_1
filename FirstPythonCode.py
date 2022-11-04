98a86eaf3ce0d579993cf9ade5ee684612a390b7
Piotr Kowalski <Piotr.Kowalski@santanderconsumer.dk>
2022-11-04 10:35:59 +0100

    Simple Calculations

--- FirstPythonCode.py
+++ FirstPythonCode.py
@@ -1,1 +1,17 @@
 print("Hello World");
+print("");
+print("Basic Calculations:");
+print(3 + 3);
+print(3 - 3);
+print(3 * 3);
+print(3 / 3);
+print (3 % 2)  # give rest of divide
+print (3 ** 4) # 3 to the fourth power
+print (3 // 4) # divide and round down
+
+print("");
+print("Variables and Calculations");
+
+a = 2;
+a += 2;
+print(a);
