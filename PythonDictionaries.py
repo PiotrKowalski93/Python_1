b76050cf1d701c2910edef433add3350521bcd4a
Piotr Kowalski <Piotr.Kowalski@santanderconsumer.dk>
2022-11-04 11:38:48 +0100

    Dictionaries

---
+++ PythonDictionaries.py
@@ -0,0 +1,17 @@
+# Dictionary 'key' : value
+# Dictionaries are not based on index
+# There is no particular order in dictionary
+myExample = {'someItem': 2, 'otherItem': 20}
+
+# similar to tables in other language
+print(myExample['otherItem'])
+
+myExample['newItem'] = 400
+
+for a in myExample:
+    print(a)
+    
+print();
+
+for a in myExample:
+    print(a, myExample[a])
