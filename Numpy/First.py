911535e542d5606974913a2d1262ba5a1dd248ea
Piotr Kowalski <piotrkow93@gmail.com>
2023-02-05 19:00:42 +0100

    Np vs List

--- Numpy/First.py
+++ Numpy/First.py
@@ -1,4 +1,5 @@
 import numpy as np
+from timeit import default_timer as timer
 
 a = np.array([1,2,3])
 
@@ -6,10 +7,41 @@ print(a)
 # Dimentions
 print(a.shape)
 print(a.dtype)
-
 # NUmber of dimentions
 print(a.ndim)
-
 print(a.size)
 # Bit size of element
 print(a.itemsize)
+
+A = np.random.randn(1000)
+B = np.random.randn(1000)
+
+def dot1():
+    dot = 0
+    for i in range(len(A)):
+        dot += A[i] * B[i]
+    return dot
+
+def dot2():
+    return np.dot(A, B)
+
+T = 1000
+
+start = timer()
+for i in range(T):
+    dot1()
+end = timer()
+
+t1 = end - start
+
+start = timer()
+for i in range(T):
+    dot2()
+end = timer()
+
+t2 = end - start
+
+print('List: ',t1)
+print('np.dot: ',t2)
+
+
