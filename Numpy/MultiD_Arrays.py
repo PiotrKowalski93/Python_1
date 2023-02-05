af03540941e3963f660a83c9ec0a957fe37f0140
Piotr Kowalski <piotrkow93@gmail.com>
2023-02-05 19:26:49 +0100

    Fancy Indexing

--- Numpy/MultiD_Arrays.py
+++ Numpy/MultiD_Arrays.py
@@ -18,3 +18,12 @@ print(np.diag(a))
 # More Slicing
 print(a[0,0:2])
 print(a[1,0:2])
+
+# Boolean indexing
+print(a[a > 4])
+b = np.where(a > 3, a, 0)
+print(b)
+
+a = np.array([1,2,3,4,5,6])
+even = np.argwhere(a%2==0).flatten()
+print(a[even])
