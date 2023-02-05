05b60aea42cfd60c6c303d9cf25e62856a619e1e
Piotr Kowalski <piotrkow93@gmail.com>
2023-02-05 19:55:22 +0100

    hstack and vstack

--- Numpy/Concatenation.py
+++ Numpy/Concatenation.py
@@ -10,3 +10,13 @@ d = np.concatenate((a,b), axis=None)
 print(d)
 e = np.concatenate((a,b.T), axis=1)
 print(e)
+
+# hstack - horizontall stack
+# vstack - verticalstack
+a1 = np.array([1,2,3])
+a2 = np.array([1,2,3])
+c1 = np.hstack((a1,a2))
+print(c1)
+
+c2 = np.vstack((a1,a2))
+print(c2)
